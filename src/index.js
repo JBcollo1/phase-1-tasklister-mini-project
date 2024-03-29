38696469b769581653b19cdb52419de5a4ad1e98
jbcollins254@gmail.com <jbcollins254@gmail.com>
2024-03-29 11:42:50 +0300

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let fom = document.getElementById("create-task-form")
+  document.addEventListener('submit', function(e) {
+  e.preventDefault()
+   let inp = document.querySelector("#new-task-description")
+  let p1 =document.createElement("li")
+  p1.textContent = `${inp.value} `
+  let tod = document.querySelector("#tasks")
+  inp.value = ""
+  tod.appendChild(p1)
+  let btn = document.createElement("button")
+  console.log(btn)
+  btn.textContent = "x"
+  p1.appendChild(btn)
+  btn.addEventListener("click",(e) => {
+   e.target.parentNode.remove()
+  })
+  })
 });
